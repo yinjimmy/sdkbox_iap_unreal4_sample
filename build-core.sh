941723bccc5b5ec2a50519235fd6712b7d544095
yinjimmy <jimmy.yin5@gmail.com>
2019-01-10 14:54:22 +0800

    fix

--- build-core.sh
+++ build-core.sh
@@ -1,5 +1,6 @@
+#!/bin/bash -e
 pushd sdkbox-core
 #./build.py -c Debug -g iap -p i --host UnrealEngine4
-./build.py -g iap --host UnrealEngine4
+./build.py -g iap --host UnrealEngine4 -t n
 popd
 echo Core Done.
